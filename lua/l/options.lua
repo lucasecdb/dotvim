a29045f189224781d40a71e8416ad09606b99602
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-05-04 16:12:22 -0300

    Remove autoindent and tweak some options

--- lua/l/options.lua
+++ lua/l/options.lua
@@ -10,7 +10,6 @@ opt('expandtab', true, buffer)
 opt('shiftwidth', 2, buffer)
 opt('tabstop', 2, buffer)
 opt('softtabstop', 2, buffer)
-opt('autoindent')
 
 -- Search
 opt('ignorecase') -- Case insensitive search
@@ -44,7 +43,8 @@ opt('showmatch') -- Hightlight matching parens
 opt('lazyredraw')  -- Redraw only when need to
 opt('laststatus', 2) -- Show 2 status lines for errors
 
-opt('scrolloff', 3) -- Keep 3 lines of distance from top and bottom scroll
+opt('scrolloff', 3) -- Lines to scroll vertically
+opt('sidescrolloff', 3) -- Lines to scroll horizontally
 
 -- UX
 opt('confirm')
@@ -59,6 +59,25 @@ opt('backspace', {'indent','eol','start'})
 opt('whichwrap', 'b,s,h,l,<,>,[,]') -- Backspace and cursor keys wrap lines
 opt('completeopt', {'noinsert', 'menuone', 'noselect'})
 
+-- Autoformatting
+opt('formatoptions', {
+  'c',  -- Auto-wrap comments
+  'a',  -- Auto format paragraph
+  '2',  -- Use the second line's indent vale when indenting (allows indented first line)
+  'q',  -- Formatting comments with `gq`
+  'w',  -- Trailing whitespace indicates a paragraph
+  'j',  -- Remove comment leader when makes sense (joining lines)
+  'r',  -- Insert comment leader after hitting Enter
+  'o',  -- Insert comment leader after hitting `o` or `O`
+}, buffer)
+
+-- Messages
+local short_mess = 'filnxtToOF'
+short_mess = short_mess .. 'W'  -- Don't print "written" when editing
+short_mess = short_mess .. 'a'  -- Use abbreviations in messages ([RO] intead of [readonly])
+short_mess = short_mess .. 'c'  -- Do not show ins-completion-menu messages (match 1 of 2)
+opt('shortmess', short_mess)
+
 -- Integration with system clipboard
 opt('clipboard', {'unnamed', 'unnamedplus'})
 
@@ -72,3 +91,9 @@ opt('cmdheight', 2)
 
 -- always show signcolumns
 opt('signcolumn', 'yes')
+
+-- Undo & History
+opt('undofile')
+opt('undolevels', 1000)
+opt('undoreload', 10000)
+opt('shada', '!,\'1000,<50,s10,h')  -- Increase the shadafile size so that history is longer
