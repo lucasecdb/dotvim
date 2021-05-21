0d14b40b81978d3b8185c9f68bebf87b09ccb1cb
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-05-21 10:35:35 -0300

    Add more parsers to tree-sitter

--- lua/l/plugins/treesitter.lua
+++ lua/l/plugins/treesitter.lua
@@ -1,3 +1,14 @@
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+parser_config.embedded_templates = {
+  install_info = {
+    url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
+    files = {"src/parser.c"}
+  },
+  filetype = "ejs",
+  used_by = {"erb"},
+}
+
 require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
@@ -21,5 +32,6 @@ require('nvim-treesitter.configs').setup {
   ensure_installed = {
     'query', 'typescript', 'javascript', 'tsx', 'json', 'rust', 'graphql', 'python',
     'jsdoc', 'lua', 'css', 'bash', 'html', 'yaml', 'jsonc', 'scss', 'toml', 'dockerfile',
+    'embedded_templates', 'elixir',
   },
 }
