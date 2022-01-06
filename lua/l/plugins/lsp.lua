28959585d08e3fbe3a7b7fff9ada78f684147d7e
Lucas Cordeiro <ecdb.lucas@gmail.com>
2022-01-06 10:03:48 -0300

    Fix use of deprecated lsp functions

--- lua/l/plugins/lsp.lua
+++ lua/l/plugins/lsp.lua
@@ -39,8 +39,8 @@ local function on_attach(client, bufnr)
     map(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
     map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
     --   Use `[g` and `]g` to navigate diagnostics
-    map(bufnr, 'n', '[g', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-    map(bufnr, 'n', ']g', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    map(bufnr, 'n', '[g', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    map(bufnr, 'n', ']g', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
     map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
     map(bufnr, 'n', '<leader>k', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
         opts) -- Use `<leader>k` to show documentation
