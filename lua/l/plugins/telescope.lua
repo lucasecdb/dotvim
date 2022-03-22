583afcb6306935ad3ea35e34b874a54623937fb7
Lucas Cordeiro <ecdb.lucas@gmail.com>
2022-03-22 17:11:55 -0300

    Show hidden files with ripgrep

--- lua/l/plugins/telescope.lua
+++ lua/l/plugins/telescope.lua
@@ -28,7 +28,7 @@ require('telescope').setup {
 
         vimgrep_arguments = {
             'rg', '--color=never', '--no-heading', '--with-filename',
-            '--line-number', '--column', '--smart-case'
+            '--line-number', '--column', '--smart-case', '--hidden'
         }
     },
     extensions = {
