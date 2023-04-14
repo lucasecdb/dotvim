307ae105416710d92ef3234c297ac8bdf7f2b313
Lucas Cordeiro <ecdb.lucas@gmail.com>
2023-04-14 10:46:07 -0300

    Show hidden files in telescope

--- lua/l/mappings.lua
+++ lua/l/mappings.lua
@@ -21,10 +21,12 @@ vim.keymap.set('n', '<leader>p', ':Git push<cr>')
 -- Fuzzy finder
 local telescope = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
+vim.keymap.set('n', '<leader>ff',
+               function() telescope.find_files {hidden = true} end, {})
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
+vim.keymap.set('n', '<leader>fv', telescope.treesitter, {})
 
 -- Misc
 vim.keymap.set('n', '<leader>q', ':q<cr>')
