3f554432471bb35a901889d706502e426d9bc0ab
Lucas Cordeiro <ecdb.lucas@gmail.com>
2023-09-14 16:52:50 +0200

    Add sections config for statusline

--- lua/l/statusline.lua
+++ lua/l/statusline.lua
@@ -3,5 +3,21 @@ require('lualine').setup({
         theme = "catppuccin",
         section_separators = {left = '', right = ''},
         component_separators = {left = '', right = ''}
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'FugitiveHead', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'searchcount', 'selectioncount', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'%f'},
+        lualine_x = {'filetype'},
+        lualine_y = {},
+        lualine_z = {}
     }
 })
