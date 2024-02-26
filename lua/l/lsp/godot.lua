d0f92484990acfd847da7ec95c350a1ccb3f3766
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-02-26 18:52:02 +0100

    Use godot-wsl-lsp instead of direct RPC connection

--- lua/l/lsp/godot.lua
+++ lua/l/lsp/godot.lua
@@ -1,21 +1,11 @@
-local uv = require 'luv'
-local configs = require 'lspconfig.configs'
+local configs = require('lspconfig.configs')
 
 -- Check if the config is already defined (useful when reloading this file)
 if not configs.godot_lsp then
     configs.godot_lsp = {
         default_config = {
             name = 'godot',
-            cmd = function(...)
-                local hostname = vim.fn.hostname() .. '.local'
-                local ip_address = os.getenv('GDScript_Address') or
-                                       uv.getaddrinfo(hostname)[1]['addr']
-
-                local port = os.getenv('GDScript_Port') or '6005'
-                local create_rpc = vim.lsp.rpc.connect(ip_address, port)
-
-                return create_rpc(unpack(arg))
-            end,
+            cmd = {'godot-wsl-lsp'},
             filetypes = {'gdscript'},
             root_dir = function(fname)
                 return vim.fs.dirname(vim.fs.find({'project.godot', '.git'}, {
@@ -23,7 +13,15 @@ if not configs.godot_lsp then
                     path = vim.fs.dirname(fname)
                 })[1])
             end,
-            settings = {}
+            handlers = {
+                ['gdscript_client/changeWorkspace'] = function(err, result)
+                    if err then return end
+
+                    local path = result['path']
+
+                    vim.fn('cd', path)
+                end
+            }
         }
     }
 end
