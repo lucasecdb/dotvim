43e8229729c9818ba2ac64953abe5e151774b3b6
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-03-15 00:12:21 +0100

    Update indentation in java filetypes

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -9,3 +9,6 @@ local jdtls_config = require 'l.lsp.java'
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
 jdtls.start_or_attach(jdtls_config)
+
+vim.go.shiftwidth = 4
+vim.go.tabstop = 4
