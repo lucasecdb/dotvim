d6d97881e4cb298977af8662618aeca82ab2c2aa
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-09-03 16:43:09 +0200

    Skip php debug adapter config if not installed

--- lua/l/plugins/dap.lua
+++ lua/l/plugins/dap.lua
@@ -1,18 +1,21 @@
 local dap = require 'dap'
+local registry = require 'mason-registry'
 
-dap.adapters.php = {
-  type = 'executable',
-  command = 'node',
-  args = {
-    vim.fs.joinpath(vim.fn.stdpath 'data' --[[@as string]], 'mason', 'packages', 'php-debug-adapter', 'extension', 'out', 'phpDebug.js'),
-  },
-}
+if registry.is_installed 'php-debug-adapter' then
+  dap.adapters.php = {
+    type = 'executable',
+    command = 'node',
+    args = {
+      vim.fs.joinpath(registry.get_package('php-debug-adapter'):get_install_path(), 'extension', 'out', 'phpDebug.js'),
+    },
+  }
 
-dap.configurations.php = {
-  {
-    type = 'php',
-    request = 'launch',
-    name = 'Listen for Xdebug',
-    port = 9003,
-  },
-}
+  dap.configurations.php = {
+    {
+      type = 'php',
+      request = 'launch',
+      name = 'Listen for Xdebug',
+      port = 9003,
+    },
+  }
+end
