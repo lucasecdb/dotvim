60e5205b446d99974d621760ab77f2350751b600
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-09-05 14:25:41 +0200

    Increase timeout for conform

--- lua/l/lazy.lua
+++ lua/l/lazy.lua
@@ -192,7 +192,7 @@ require('lazy').setup {
     opts = {
       notify_on_error = false,
       format_on_save = {
-        timeout_ms = 1000,
+        timeout_ms = 5000,
         lsp_fallback = true,
       },
       formatters_by_ft = {
