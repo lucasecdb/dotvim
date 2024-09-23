93cb6765d282435b05c277887baf65e1c7d7518c
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-09-23 15:37:16 +0200

    Remove nvim-lint

--- lua/l/lazy.lua
+++ lua/l/lazy.lua
@@ -114,18 +114,6 @@ require('lazy').setup {
     end,
   },
 
-  -- Lint
-  {
-    'mfussenegger/nvim-lint',
-    config = function()
-      vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
-        callback = function()
-          require('lint').try_lint()
-        end,
-      })
-    end,
-  },
-
   -- DAP
   {
     'rcarriga/nvim-dap-ui',
