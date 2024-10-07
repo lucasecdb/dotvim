1d42bade2d0c8d70bdcda517bb93e2e08f6feae9
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-10-07 14:21:30 +0200

    Add basic config for JS DAP

--- lua/l/plugins/dap.lua
+++ lua/l/plugins/dap.lua
@@ -22,3 +22,37 @@ if registry.is_installed 'php-debug-adapter' then
     },
   }
 end
+
+if registry.is_installed 'js-debug-adapter' then
+  local install_path = registry.get_package('js-debug-adapter'):get_install_path()
+
+  dap.adapters['pwa-node'] = {
+    type = 'server',
+    host = 'localhost',
+    port = '${port}',
+    executable = {
+      command = 'node',
+      args = { install_path .. '/js-debug/src/dapDebugServer.js', '${port}' },
+    },
+  }
+
+  local vitest_config = {
+    type = 'pwa-node',
+    request = 'launch',
+    name = 'Debug Current Test File',
+    autoAttachChildProcesses = true,
+    skipFiles = { '<node_internals>/**', '**/node_modules/**' },
+    program = '${workspaceFolder}/node_modules/vitest/vitest.mjs',
+    cwd = '${workspaceFolder}',
+    args = { 'run', '${file}' },
+    smartStep = true,
+    console = 'neverOpen',
+  }
+
+  dap.configurations.javascript = {
+    vitest_config,
+  }
+  dap.configurations.typescript = {
+    vitest_config,
+  }
+end
