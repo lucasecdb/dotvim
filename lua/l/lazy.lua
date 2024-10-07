995e7eea9eccb571370aa3c4808ab1d00131a900
Lucas Cordeiro <ecdb.lucas@gmail.com>
2024-10-07 13:36:45 +0200

    add signature help plugin

--- lua/l/lazy.lua
+++ lua/l/lazy.lua
@@ -223,6 +223,7 @@ require('lazy').setup {
     end,
     dependencies = {
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
--- lua/l/plugins/cmp.lua
+++ lua/l/plugins/cmp.lua
@@ -48,6 +48,7 @@ cmp.setup {
   },
   sources = cmp.config.sources {
     { name = 'nvim_lsp' },
+    { name = 'nvim_lsp_signature_help' },
     { name = 'luasnip' },
     { name = 'buffer' },
     { name = 'path' },
