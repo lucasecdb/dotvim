a83494f3ec1a2900da6d1bf9a7df3dee218a20ba
Lucas Cordeiro <ecdb.lucas@gmail.com>
2026-05-28 11:14:57 +0200

    Fix lualine theme

--- lua/l/plugins/lualine.lua
+++ lua/l/plugins/lualine.lua
@@ -10,7 +10,7 @@ end
 
 require('lualine').setup {
   options = {
-    theme = 'catppuccin',
+    theme = 'catppuccin-nvim',
     section_separators = { left = '', right = '' },
     component_separators = { left = '', right = '' },
   },
